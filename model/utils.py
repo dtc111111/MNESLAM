b634ba5dea987f1a4d7ee2d8c8f6283f37719d00
SHEN GUOLE <mikrokosmos@sjtu.edu.cn>
2025-06-10 14:54:05 +0800

    Update utils.py

--- model/utils.py
+++ model/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from math import exp, log, floor
-####eslam#########
+
 def get_rays(H, W, fx, fy, cx, cy, c2w, device):
     """
     Get rays for a whole image.
